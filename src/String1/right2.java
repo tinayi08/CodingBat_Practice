c26f00534fce30fa5024517ca9e1925413cc5ac9
tinayi08 <christinayi827@gmail.com>
2021-04-05 20:49:48 -0700

    exercise completed

--- src/String1/right2.java
+++ src/String1/right2.java
@@ -9,6 +9,7 @@ public class right2 {
 //    right2("Hello") → "loHel"
 //    right2("java") → "vaja"
 //    right2("Hi") → "Hi"
+
     public String right2(String str) {
         String right = str.substring(str.length()-2, str.length());
         String remaining = str.substring(0,str.length()-2);
