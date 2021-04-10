f5f660037577b569b6c4063a5b0274eb9f96be4a
tinayi08 <christinayi827@gmail.com>
2021-04-10 15:36:53 -0700

    exercise complete

---
+++ src/String1/endsLy.java
@@ -0,0 +1,27 @@
+package String1;
+
+public class endsLy {
+
+
+//    Given a string, return true if it ends in "ly".
+//
+//
+//    endsLy("oddly") → true
+//    endsLy("y") → false
+//    endsLy("oddy") → false
+
+    public boolean endsLy(String str) {
+        if (str.length() < 2) {
+            return false;
+        }
+        String ly = str.substring(str.length()-2, str.length());
+        if(ly.equalsIgnoreCase("ly")) {
+
+            return true;
+        }
+
+        return false;
+
+    }
+
+}
