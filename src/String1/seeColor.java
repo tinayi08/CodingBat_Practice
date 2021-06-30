14faaa1e2f833489b2936f670f6d19f9b6d25df5
tinayi08 <christinayi827@gmail.com>
2021-06-29 22:43:33 -0700

    completed seeColor

---
+++ src/String1/seeColor.java
@@ -0,0 +1,23 @@
+package String1;
+
+public class seeColor {
+
+
+//    Given a string, if the string begins with "red" or "blue" return that color string,
+//    otherwise return the empty string.
+//
+//    seeColor("redxx") → "red"
+//    seeColor("xxred") → ""
+//    seeColor("blueTimes") → "blue"
+
+
+    public String seeColor(String str) {
+        if (str.startsWith("red")) {
+            return "red";
+        } else if (str.startsWith("blue")) {
+            return "blue";
+        } else
+            return "";
+    }
+
+}
