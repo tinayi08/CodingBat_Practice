8ac77b38f85b9558c08a5064fdc9614eea70c8d6
tinayi08 <christinayi827@gmail.com>
2021-07-11 16:04:48 -0700

    exercise completed

---
+++ src/String1/without2.java
@@ -0,0 +1,30 @@
+package String1;
+
+public class without2 {
+
+
+//    Given a string, if a length 2 substring appears at both its beginning
+//    and end, return a string without the substring at the beginning,
+//    so "HelloHe" yields "lloHe". The substring may overlap with itself,
+//    so "Hi" yields "". Otherwise, return the original string unchanged.
+//
+//
+//    without2("HelloHe") → "lloHe"
+//    without2("HelloHi") → "HelloHi"
+//    without2("Hi") → ""
+
+    public String without2(String str) {
+        if (str.length() < 2) {
+            return str;
+        }
+        String front = str.substring(0,2);
+        String end = str.substring(str.length()-2,str.length());
+        if (front.equalsIgnoreCase(end)) {
+            return str.substring(2,str.length());
+        } else
+            return str;
+
+    }
+
+
+}
